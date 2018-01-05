57c69752abfd988019a7c2dee778557d0fa82023
Gilson Gil <gilson.gil@me.com>
2018-01-05 21:14:54 -0200

    Initial commit

---
+++ Easyntest/Application/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Easyntest
+//
+//  Created by Gilson Gil on 05/01/18.
+//  Copyright © 2018 Gilson Gil. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+}
